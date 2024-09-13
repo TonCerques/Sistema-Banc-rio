32afd9f84f7677388957a55339bb17ad39dceeff
TonCerques <zcoltyy@gmail.com>
2024-09-13 15:35:54 -0300

    feat: Extends

--- src/conta.java
+++ src/conta.java
@@ -1,5 +1,58 @@
- public class conta extends pessoa
+        public class conta extends pessoa
 
- private double deposito;
- private double saque;
- private 
+        private double valorsaque;
+        private double valordepo;
+        private double saldo;
+        private double transf;
+
+        conta (){}
+
+        conta (double saque, double depo, double saldo, double transf){
+            this.saldo = saldo;
+            this.saque = saque;
+            this.transf = transf;
+            this.valordepo = depo;
+} 
+            public double getValorsaque() {
+                            return valorsaque;
+            }
+
+            public void setValorsaque(double valorsaque) {
+                this.valorsaque = valorsaque;
+            }
+
+            public double getValordepo() {
+                return valordepo;
+            }
+
+            public void setValordepo(double valordepo) {
+                this.valordepo = valordepo;
+            }
+
+            public double getSaldo() {
+                return saldo;
+            }
+
+            public void setSaldo(double saldo) {
+                this.saldo = saldo;
+            }
+
+            public double getTransf() {
+                return transf;
+            }
+
+            public void setTransf(double transf) {
+                this.transf = transf;
+            }
+            // FIM GETTERS AND SETTERS
+            public void saque (){
+
+                0
+
+            }
+
+    
+
+
+ }
+ 
